68a5381f2ff632eeb3d49494c425df8dafbd0404
liuxing <13366209573@163.com>
2017-03-09 09:36:51 +0800

    修改

--- static/orders.js
+++ static/orders.js
@@ -12,13 +12,16 @@ var ordersVue = new Vue({
   el: "#order-list",
 
   data: {
-    orders: []
+    orders: [],
+	askArr: [],
+	bidArr: []  
   },
 
   init: this.fetchOrders,
 
   ready: function() {
-    setInterval(this.fetchOrders, 2000);
+  	this.fetchOrders();
+    
   },
 
   methods: {
@@ -33,15 +36,52 @@ var ordersVue = new Vue({
           "size": "100"
         },
 	    success: function(resp) {
-	      console.log(resp);
-	      self.orders = resp;
+			self.rsapHandle(resp);		
+//	      	self.orders = resp;
 	    },
 	    error: function(jqXHR, exception) {
 	      console.log("Failed to get chain height!");
           self.orders = [];
 	    }
       });
-    }
+    },
+	rsapHandle: function(data){
+		var self = this;
+		data.forEach(function(order){
+			if(order.side == 'ask'){
+				self.askArr.push(order)
+			}else{
+				self.bidArr.push(order)
+			};
+		});
+		self.askArr.sort(self.sortFun('price','quantity'));
+		self.bidArr.sort(self.sortFun('price','quantity'));
+		self.askArr.reverse();
+		self.bidArr.reverse();
+		self.askArr.length = self.bidArr.length = 20;
+		self.orders = self.askArr.concat(self.bidArr)
+	},
+	sortFun: function (price,num){
+		return function(frontObj,behindObj){
+			var frontPrice = Number(frontObj.price),
+				frontNum = Number(frontObj.number),
+				behindPrice = Number(behindObj.price),
+				behindNum = Number(behindObj.number);
+			if(frontPrice > behindPrice){
+				return 1
+			}else if(frontPrice < behindPrice){
+				return -1
+			}else{
+				if(frontNum > behindNum){
+					return 1
+				}else if(frontNum < behindNum){
+					return -1
+				}else{
+					return 0
+				}
+			}
+		}	
+	}  
   }
 });
 
